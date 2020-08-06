d69bfb6b5b518a17217425edf61b3413ae51dc4a
aynurerkin <64187339+aynurerkin@users.noreply.github.com>
2020-08-05 18:33:43 -0700

    Update GitFun.java

--- src/Utility/GitFun.java
+++ src/Utility/GitFun.java
@@ -3,5 +3,6 @@ package Utility;
 public class GitFun {
     public static void main(String[] args) {
         System.out.println("Git is fun.");
+        System.out.println("made some changes");
     }
 }
